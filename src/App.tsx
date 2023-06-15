ed3b928af8e43658fa402360c526de4fc013b77f
Wagner Terry <goncalveswagner15@gmail.com>
2023-06-14 22:50:49 -0300

    feat: adicionando imagem / splash ao carregar app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback, useEffect, useState } from "react";
 import {Text, View} from  'react-native'
+import {
+    useFonts,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold
+} from "@expo-google-fonts/poppins"
+import {DMSans_400Regular} from '@expo-google-fonts/dm-sans'
+import {DMSerifDisplay_400Regular} from "@expo-google-fonts/dm-serif-display"
 
-export const App: React.FC = () => {
+SplashScreen.preventAutoHideAsync();
+
+const App: React.FC = () => {
+    const [fontsLoaded, setFontsLoaded] = useFonts({
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold,
+        Poppins_800ExtraBold,
+        DMSans_400Regular,
+        DMSerifDisplay_400Regular
+    })
+
+    useEffect(() => {
+        async function loadFonts(){
+            if(!fontsLoaded){
+                await SplashScreen.hideAsync();
+            }
+        }
+        loadFonts()
+    })
+
     return (
-        <View>
-            <Text>Wallet App</Text>
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <Text>Wallet Apps</Text>
         </View>
     )
 }
+
+export default App;
