162cec4c4f3f3921928fd131361de5173c88f656
Wagner Terry <goncalveswagner15@gmail.com>
2023-06-15 11:51:02 -0300

    chore: iniciando tela de de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import * as SplashScreen from 'expo-splash-screen';
 import React, { useEffect } from "react";
-import {Text, View} from  'react-native'
-import {ThemeProvider} from 'styled-components/native'
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { ThemeProvider } from "styled-components/native";
 import {
     useFonts,
     Poppins_300Light,
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_700Bold,
-    Poppins_800ExtraBold
-} from "@expo-google-fonts/poppins"
-import {DMSans_400Regular} from '@expo-google-fonts/dm-sans'
-import {DMSerifDisplay_400Regular} from "@expo-google-fonts/dm-serif-display"
+    Poppins_800ExtraBold,
+} from "@expo-google-fonts/poppins";
+import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 
-import COLORS from '../src/styles/theme'
+import COLORS from "../src/styles/theme";
+
+import { Login } from "./screens/Login/Login";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -25,31 +28,26 @@ const App: React.FC = () => {
         Poppins_700Bold,
         Poppins_800ExtraBold,
         DMSans_400Regular,
-        DMSerifDisplay_400Regular
-    })
+        DMSerifDisplay_400Regular,
+    });
 
     useEffect(() => {
-        async function loadFonts(){
-            if(!fontsLoaded){
+        async function loadFonts() {
+            if (!fontsLoaded) {
                 await SplashScreen.hideAsync();
             }
         }
-        loadFonts()
-    })
+        loadFonts();
+    });
 
     return (
         <ThemeProvider theme={COLORS}>
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
-        >
-            <Text>Wallet Apps</Text>
-        </View>
+            <StatusBar style="dark" translucent backgroundColor="transparent" />
+            <View>
+                <Login />
+            </View>
         </ThemeProvider>
-    )
-}
+    );
+};
 
 export default App;
